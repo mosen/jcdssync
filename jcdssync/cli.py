54341d52fe2e2112f35a0bc7101d30b043cbb914
Mosen <mosen@users.noreply.github.com>
2018-12-17 20:53:39 +1100

    Initial commit of project structure

---
+++ jcdssync/cli.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source")
+    parser.add_argument("destination")
+    parser.parse_args()
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+setup(
+    name="jcdssync",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'jcdssync = jcdssync.cli:main'
+        ]
+    },
+    # install_requires=[
+    #     ''
+    # ],
+    dependency_links=[
+        'git+https://github.com/jssimporter/python-jss.git#egg=jss-2.0.1'
+    ],
+    author="mosen",
+    author_email="mosen@noreply.users.github.com",
+    description="JCDSSync is a command line utility for syncing content to, and from, "
+                "a JAMF Pro JCDS Distribution Point",
+    license="MIT",
+    url="https://github.com/mosen/jcdssync"
+)
+
